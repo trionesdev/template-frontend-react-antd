ead8ce4656c3b4d27f59728a6e1a7e9c3cb80b37
bane.shi <fengxiaotx@163.com>
2024-09-21 23:23:37 +0800

    feat: init

--- src/app/account/sign-in/AccountSignIn.tsx
+++ src/app/account/sign-in/AccountSignIn.tsx
@@ -1,13 +1,17 @@
-import {Form, Input} from "antd";
+import {Button, Form, Input} from "antd";
+import {LockOutlined, UserOutlined} from "@ant-design/icons";
 
 export const AccountSignIn = () => {
     const [form] = Form.useForm();
-    return <Form form={form} layout={"vertical"}>
+    return <Form form={form} layout={"vertical"} size={`large`}>
         <Form.Item name="username">
-            <Input placeholder={`请输入用户名`}/>
+            <Input prefix={<UserOutlined/>} placeholder={`请输入用户名`}/>
         </Form.Item>
         <Form.Item name="password">
-            <Input.Password placeholder={`请输入密码`}/>
+            <Input.Password prefix={<LockOutlined/>} placeholder={`请输入密码`}/>
+        </Form.Item>
+        <Form.Item>
+            <Button type={`primary`} block={true}>登录</Button>
         </Form.Item>
     </Form>
 }
